207aeb3768d7fbc3722aca34e75dd13f2a7b60c0
TwTYwY <aaignatova@edu.hse.ru>
2026-02-10 18:17:03 +0300

    HW: Point 3

--- aaignatovaPW5/Models/ArticleModel.swift
+++ aaignatovaPW5/Models/ArticleModel.swift
@@ -7,22 +7,29 @@
 
 import Foundation
 
-struct ArticleModel {
-    let id: UUID
-    let title: String
-    let description: String
-    let publishedAt: Date
-    let source: String
-    let url: URL?
-    let imageUrl: URL?
-    
-    init(title: String, description: String, source: String, url: URL? = nil, imageUrl: URL? = nil) {
-        self.id = UUID()
-        self.title = title
-        self.description = description
-        self.publishedAt = Date()
-        self.source = source
-        self.url = url
-        self.imageUrl = imageUrl
+struct ArticleModel: Decodable {
+    var newsId: Int?
+    var title: String?
+    var announce: String?
+    var img: ImageContainer?
+    var requestId: String?
+    var articleUrl: URL? {
+        let requestId = requestId ?? ""
+        let newsId = newsId ?? 0
+        return URL(string: "https://news.myseldon.com/ru/news/index/\(newsId)?requestId=\(requestId)")
+    }
+}
+
+struct ImageContainer: Decodable {
+    var url: URL?
+}
+
+struct NewsPage: Decodable {
+    var news: [ArticleModel]?
+    var requestId: String?
+    mutating func passTheRequestId() {
+        for i in 0..<(news?.count ?? 0) {
+            news?[i].requestId = requestId
+        }
     }
 }
